d56ca61879ea8344e1bbf517e6e4a58d2c5d0e48
Nick Graffis <nicholasgraffis@gmail.com>
2021-02-20 11:02:31 -0800

    🐛 w/ CORS

--- controllers/tokensController.js
+++ controllers/tokensController.js
@@ -14,6 +14,7 @@ module.exports = {
       }
       if (Date.now() > t.exp) {
         res.status(200).json({message: 'Token has expired!'})
+        return
       }
 
       res.status(200).json(t)
