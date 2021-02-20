36a2f909cbc8d96228eab28c01e9170b5af954c0
Nick Graffis <nicholasgraffis@gmail.com>
2021-02-20 14:05:00 -0800

    CORS

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const session = require('express-session');
 const routes = require('./routes');
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.options('*', cors())
 const socket = require('socket.io');
 const mongoCreate = require('./utils/mongoCreate.js')
 //hello
